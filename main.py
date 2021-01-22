b1627c7d7bbb1bc1173fcb54f138985ffd206297
Terrell Cooper <TerrellCoop@gmail.com>
2021-01-22 12:38:57 -0700

    finshed app but still a bug with the recording

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ RUN_TOKEN = "tYB5B3Dawx_p"
 
 response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
 data = json.loads(response.text)
-# print(data["total"])
+
 
 class Data: 
     def __init__(self, api_key, project_token):
@@ -22,7 +22,7 @@ class Data:
         self.get_data()
 
     def get_data(self):
-        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
+        response = requests.get('https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
         self.data = json.loads(response.text)
     
     def get_total_cases(self):
@@ -42,33 +42,92 @@ class Data:
         return "0"
 
     def get_country_data(self, country):
-        data = self.data['country']
+        data = self.data['country]'
         
         for content in data:
             if content['name'].lower() == country.lower():
                 return content
             
         return "0"
+    
+    def get_list_of_countries(self):
+        countries = []
+        for country in self.data['country']:
+            countries.append(country['name'].lower())
+        
+        return countries
 
 
 data = Data(API_KEY, PROJECT_TOKEN)
+# print(data.get_list_of_countries())
 
 def speak(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
-def get_audio():
+def get_audio(): 
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
-        said = ""
+        said = ''
         
         try:
             said = r.recognize_google(audio)
-        except Exception as e: 
-            print("Exception:", str(e))
-    
+        except Exception as e:
+            print('Exception', str(e))
+            
     return said.lower()
-
 print(get_audio())
+
+def main():
+    print("Started Program")
+    data = Data(API_KEY, PROJECT_TOKEN)
+    END_PHRASE = "stop"
+    country_list = data.get_list_of_countries()
+
+    TOTAL_PATTERNS = {
+        re.compile("[\w\s]+ total [\w\s]+ cases"):data.get_total_cases,
+		re.compile("[\w\s]+ total cases"): data.get_total_cases,
+        re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
+        re.compile("[\w\s]+ total deaths"): data.get_total_deaths
+}
+
+    COUNTRY_PATTERNS = {
+        re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+        re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],
+}
+
+    UPDATE_COMMAND = "update"
+
+    while True:
+        print("Listening...")
+        text = get_audio()
+        print(text)
+        result = None
+
+        for pattern, func in COUNTRY_PATTERNS.items():
+            if pattern.match(text):
+                words = set(text.split(" "))
+                for country in country_list:
+                    if country in words:
+                        result = func(country)
+                        break
+
+        for pattern, func in TOTAL_PATTERNS.items():
+            if pattern.match(text):
+                result = func()
+                break
+
+        if text == UPDATE_COMMAND:
+            result = "Data is being updated. This may take a moment!"
+            data.update_data()
+
+        if result:
+            speak(result)
+
+        if text.find(END_PHRASE) != -1:  # stop loop
+            print("Exit")
+            break
+
+main()
